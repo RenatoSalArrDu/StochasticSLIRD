3e3228706522566b33fcc3e395fb08ed1a77d339
Renato Salomon Arroyo Duarte <salomonarroyo@gmail.com>
2022-07-27 22:16:58 -0500

    [FEAT:refactor] refactoring code

--- run.sh
+++ run.sh
@@ -1,22 +1,43 @@
 #!//bin/bash
 #$ -S /bin/bash
 
-# ============= Epidemiological Parameters =================!
-ni=10  # initial infectious (this amount is estimated by adding the number of the new infected in the empirical data, until every day is at least one new infected reported)
-ns=9999990  # initial susceptible
-nr=0      # initial recovered
-nl=0      # initial latent
-nd=0      # initial dead
+# =============================================================================!
+# Mexican states to simulate, (from Feb the 23th 2021)
+#camp, cdmx, chis, jal, edmx, mich, nay, nl, oax
+state=cdmx #state
+date=20_08_21 # final date to simulate day_month_year
 
+# ============= Simulation Parameters =================!
+# === see initial condition reference file
+ni=33   # initial infectious (this amount is estimated to fit real data)
+ns=9999990  # initial susceptible (not necesary unless herd immunity is simulated)
+nr=0      # initial recovered
+nd=0      # initial dead
+icum=117  #initial cumulative of incidence
+
+nltm4=6      # latent at t0 - 4
+nltm3=7      # latent at t0 - 3
+nltm2=8      # latent at t0 - 2
+nltm1=21     # latent at t0 - 1
+
+tshf=26    # initial shift in time to beging cases from Feb the 23th 2021
+ntot=540   # number of time units (total number of days)
+traj=1000  # number of trajectories
+
+# ======= COVID-19 Parameters ============#
 t_r=18      # time of recovery (each t_recov cycles we recover)
 t_l=4       # time of latency ( each t_laten cycles we infect)
 ro=4        # basic rep. num.
 leth=0.1    # fatality
 
-# =============== Simulation Parameters ===================!
-ntot=540    # number of time units (total number of days)
-traj=10   # number of trajectories
 
+# Additional options for simulations ==========================================!
+#==============================================================================!
+#----------------- Fluctuations around Ro
+rop=0 # rop=0 --> no fluctuations,
+      # rop=1 --> uniform distro fluctuations
+      # rop=1 --> exp distro fluctuations
+      # rop=2 --> poiss distro fluctuations
 
 #-----------------   Herd immunity function (only for wop=0)
 # i) hop=1:  No herd immunity
@@ -40,21 +61,15 @@ opt=1 #opt 0 write all trajectories, else not
 wopt=1 # wopt=0 conceptual features, wopt=1 simulation of real cases based on empirical wt
 
 # ==========================================!
-# Mexican states to simulate, (from Feb the 23th 2021)
-#camp, cdmx, chis, jal, edmx, mich, nay, nl, oax
-state=cdmx #state
-date=20_08_21 # final date to simulate day_month_year
 
 data=Mexico_covid-19_datas
 iest=initial_estimation_states
-runs=<Imput path to running folder>/StochasticSLIRD
+runs=<Imput path to running folder>/StochasticSLIRD/
 
-
-
-# ==> Internal definitions  #############################################################################
-#========================================================================================================
-#========================================================================================================
-#========================================================================================================
+# ==> Internal definitions  ####################################################
+#===============================================================================
+#===============================================================================
+#===============================================================================
 
 
 #== switch option for simulation of states or not (there is no point of herd immunity under confinement;
@@ -64,26 +79,6 @@ if [ $cop == 4 ]; then
   wop=1  # simulation of real cases
 else
   wop=0  # simulation of concenptual behavior
-fi
-
-
-#== initial shift in time to beging all cases from Feb the 23th 2021 (fixed parameter seen from the data files ****)
-if [ $state == 'camp' ]; then
-  tshf=48
-elif [ $state == 'cdmx' ]; then
-  tshf=18
-elif [ $state == 'chis' ]; then
-  tshf=39
-elif [ $state == 'edmx' ]; then
-  tshf=20
-elif [ $state == 'jal' ]; then
-  tshf=25
-elif [ $state == 'mich' ]; then
-  tshf=35
-elif [ $state == 'nay' ]; then
-  tshf=46
-elif [ $state == 'oax' ]; then
-  tshf=35
 fi
 
 #========================================================================================================
@@ -96,16 +91,16 @@ r='_r'
 d='_d'
 tr='_tr'
 tl='_tl'
-rop='_Ro'
+ropp='_Ro'
 let='_let'
 cf='cfun'
 hopp='_hop'
 tra='ntrj'
 
 if [ $cop == 4 ]; then
- f=$state$tra$traj$i$ni$s$ns$l$nl$r$nr$d$nd$tr$t_r$tl$t_l$rop$ro$let$leth
+ f=$state$tra$traj$i$ni$s$ns$l$nl$r$nr$d$nd$tr$t_r$tl$t_l$ropp$ro$rop$let$leth
 else
- f=$tra$traj$cf$cop$i$ni$s$ns$l$nl$r$nr$d$nd$tr$t_r$tl$t_l$rop$ro$let$leth
+ f=$tra$traj$cf$cop$i$ni$s$ns$l$nl$r$nr$d$nd$tr$t_r$tl$t_l$ropp$ro$rop$let$leth
 fi
 
 code=$runs/code
@@ -118,11 +113,11 @@ cp $code/exe $rundirh
 cp $data/datas$date/$state.dat $rundirh/state.dat
 cp $iest/$date/$state.dat $rundirh/wpar.dat
 cd $rundirh
-printf "$ni $ns $nr $nl $nd $t_r $t_l $ntot $ro $hop $leth $cop $traj $tshf $opt $wop" > par.dat
+printf "$ni $ns $nr $nltm4 $nltm3 $nltm2 $nltm1 $nd $t_r $t_l $ntot $ro $hop $leth $cop $traj $tshf $opt $wop $rop" > par.dat
 ./exe
 
 
-# python's code to generate relevant figures of the simulation
+# python's code to generate a relevant representation of the simulation
 printf "import numpy as np
 import matplotlib.pyplot as plt
 import pylab
@@ -171,9 +166,9 @@ plt.subplots_adjust(left=0.1, right=0.95,top=0.95, bottom=0.13)
 
 
 plt.subplot(221)
-plt.plot(dat4[:,0]+$tshf,cumsts[:,0],color = colormap(normalize(6)),label='cumulative', linewidth=2)
-plt.plot(dat5[:,0]+$tshf,dat5[:,1],color = colormap(normalize(1)),label='q1', linewidth=2)
-plt.plot(dat5[:,0]+$tshf,dat5[:,2],color = colormap(normalize(2)),label='q3', linewidth=2)
+plt.plot(dat4[:,0]+$tshf,cumsts[:,0]+$icum,color = colormap(normalize(6)),label='cumulative', linewidth=2)
+plt.plot(dat5[:,0]+$tshf,dat5[:,1]+$icum,color = colormap(normalize(1)),label='q1', linewidth=2)
+plt.plot(dat5[:,0]+$tshf,dat5[:,2]+$icum,color = colormap(normalize(2)),label='q3', linewidth=2)
 plt.plot(dato[:,0],cumst[:,0],color = 'k',label='cumultive data', linewidth=2)
 plt.tick_params(labelsize='30',labeltop=False, labelright=False, labelbottom=False)
 plt.legend(borderpad=0.1, shadow = True, loc='upper left', ncol=1,fontsize=(10))
